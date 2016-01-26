c659895697be4335ac5872c0be94f08b7e6e145a
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-01-26 08:52:52 -0800

    Added WorkflowDetail DAO

---
+++ CRM/Workflow/DAO/WorkflowDetail.php
@@ -0,0 +1,14 @@
+<?php
+
+class CRM_Workflow_DAO_WorkflowDetail extends CRM_Core_DAO {
+
+  /**
+   * static instance to hold the table name
+   *
+   * @var string
+   * @static
+   */
+  static $_tableName = 'civicrm_simple_workflow_detail';
+
+
+}
