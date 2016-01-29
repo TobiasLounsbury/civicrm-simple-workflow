c17b29018129d16011160a3c85684c33e20f325f
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-01-29 11:57:46 -0800

    Updated method to load each Step

--- js/workflow_steps.js
+++ js/workflow_steps.js
@@ -1,3 +1,6 @@
+/**
+ * Updates the Sortable Area after new elements have been added
+ */
 function SWRefreshSortable() {
   CRM.$( "#SortableDetails" ).sortable({
     placeholder: 'PlaceHolder',
@@ -10,35 +13,180 @@ function SWRefreshSortable() {
   CRM.$(".handle").disableSelection();
 }
 
+/**
+ * Updates the Order input item Elements for each
+ * Step when they are reordered, to maintain data
+ * integrity
+ */
 function SWReorderSteps() {
   CRM.$("#Data .Detail").each(function (i) {
     CRM.$(this).find(".order").val(i + 1);
   });
 }
 
+/**
+ * This function composes a function name from a
+ * prefix and step type then searches a number of
+ * name spaces to see if that function exists. If
+ * it does, a reference is returned, otherwise false.
+ *
+ * @param prefix
+ *  The type of function being looked for
+ * @param type
+ *  The step type you want to take this action for.
+ * @returns
+ *  Function or (bool)False
+ */
+function SWGetStepFunction(prefix, type) {
+  var func = prefix + type.charAt(0).toUpperCase() + type.substr(1);
+
+  if(CRM.hasOwnProperty("SimpleWorkflow") && CRM.$.isFunction(CRM.SimpleWorkflow[func])) {
+    return CRM.SimpleWorkflow[func];
+  }
+
+  if(CRM.$.isFunction(CRM[func])) {
+    return CRM[func];
+  }
+
+  if(CRM.$.isFunction(window[func])) {
+    return window[func];
+  }
+
+  return false;
+}
+
+/**
+ * This function handles click events to toggle the visibility
+ * of the step detail pane.
+ */
+function SWToggleStepDetails(event) {
+  var obj = CRM.$(event.target).parent().find(".crm-simple-workflow-step-details");
+  if(obj.hasClass("SWDetailVisible")) {
+    CRM.$(event.target).html(ts("Show Details"));
+    obj.removeClass("SWDetailVisible");
+    obj.slideUp();
+  } else {
+    CRM.$(event.target).html(ts("Hide Details"));
+    obj.addClass("SWDetailVisible");
+    obj.slideDown();
+  }
+}
+
+/**
+ * Click Handler for the delete "button" on the individual
+ * Step detail elements.
+ */
 function SWDeleteStep(event) {
-  CRM.$(event.target).parent().remove();
+  var obj = CRM.$(event.target).parent();
+  var stepType = obj.find(".entity_table").val();
+  var func = SWGetStepFunction("SimpleWorkflowStepDelete", stepType);
+  if(func) {
+    if(func(obj)) {
+      obj.remove();
+    }
+  } else {
+    obj.remove();
+  }
   SWReorderSteps();
 }
 
+/**
+ *
+ *
+ * @param obj
+ *  A reference to the new Step/Detail DOM element
+ *  that is being created
+ * @param index INT
+ *  The data index (unique int) used to keep any data from overlapping
+ * @param data
+ *  The data to set within each input/textarea
+ */
+function SWSetIndexAndData(obj, index, data) {
+  obj.find("input,textarea").each(function() {
+    var inp = CRM.$(this);
+    var data_name = inp.attr("name").replace(/.*\[([^\[]*)\]$/g, "$1");
+
+    if (data.hasOwnProperty(data_name)) {
+      inp.val(data[data_name]);
+    }
+
+    if (data.hasOwnProperty("options") && data.options !== null && data.options.hasOwnProperty(data_name)) {
+      inp.val(data.options[data_name]);
+    }
+  });
+}
+
+/**
+ * This function takes a type of step, and calls it's
+ * constructor function to compose the element, update
+ * data and insert it into the sortable pane.
+ *
+ * @param stepType
+ *  The type of step this is.
+ * @param data
+ * The data to insert into this step element
+ */
+function SWAddStep(stepType, data) {
+  if (data === null) {
+    data = {};
+  }
+  var func = SWGetStepFunction("SimpleWorkflowStepAdd", stepType.charAt(0).toUpperCase() + stepType.substr(1));
+  if(func) {
+    var template = CRM.$("#SimpleWorkflowTypeTemplateDefault").clone();
+    template.removeAttr("id");
+    if(func(template, CRM.vars.SimpleWorkflow.nextIndex, data)) {
+      SWSetIndexAndData(template, CRM.vars.SimpleWorkflow.nextIndex, data);
+      CRM.vars.SimpleWorkflow.nextIndex++;
+      CRM.$("#SortableDetails").append(template);
+    }
+  }
+
+  SWRefreshSortable();
+  SWReorderSteps();
+}
+
+
+/**
+ * Initial Page setup
+ */
 CRM.$(function ($) {
 
-  $(".crm-simple-workflow-steps-form").on("click", ".DeleteStep", SWDeleteStep);
+  //Set the initial Index to 0.
+  //This is used to make sure we don't get any overlap of data
+  //structures
+  CRM.vars.SimpleWorkflow.nextIndex = 0;
 
+  //Wire up all the delete buttons
+  $(".crm-simple-workflow-steps-form").on("click", ".DeleteStep", SWDeleteStep);
+  //Wire up all the toggle detail buttons
+  $(".crm-simple-workflow-steps-form").on("click", ".SWToggleDetails", SWToggleStepDetails);
+  //Wire up all the AddStep buttons
+  $("#AddSteps").on("click", ".AddStepButton", function(event) {
+    var stepType = CRM.$(event.target).data("step-type");
+    SWAddStep(stepType, {});
+  });
+
+  //Wire up the Save Workflow button
   $("#SaveDetails").click(function(e) {
     CRM.api3('Workflow', 'save', {
       "data": $("#Data").serialize(),
       "wid": CRM.vars.SimpleWorkflow.wid
     }).done(function(result) {
       if (!result.is_error) {
-        CRM.alert("All changes have been saved", "Saved", "success");
+        CRM.alert(ts("All changes have been saved"), ts("Saved"), "success");
         window.location = CRM.url("civicrm/workflows");
       } else {
-        CRM.alert("There was an error saving your changes.<br />" + result.error_message, "Error", "error");
+        CRM.alert(ts("There was an error saving your changes") + ".<br />" + result.error_message, ts("Error"), "error");
       }
     });
   });
 
+  //Load the Data
+  for(var i in CRM.vars.SimpleWorkflow.details) {
+    SWAddStep(CRM.vars.SimpleWorkflow.details[i].entity_table, CRM.vars.SimpleWorkflow.details[i]);
+  }
+
+  //Setup the sortable.
   SWRefreshSortable();
 });
 
--- templates/CRM/Workflow/Page/Steps.tpl
+++ templates/CRM/Workflow/Page/Steps.tpl
@@ -1,42 +1,16 @@
 {if $workflow}
 
+    <div id="AddSteps">
     {foreach from=$uiTemplates item=templatePath}
         <div class="crm-simple-workflow-ui-template">
             {include file=$templatePath location="bottom"}
         </div>
     {/foreach}
+    </div>
 
     <h3>Sort Pages</h3>
     <form id="Data" class="crm-simple-workflow-steps-form">
-
-    <div id="SortableDetails">
-    {foreach from=$details item=d}
-        {if $d.entity_table eq "Page"}
-            </div>
-            <hr/>
-        {/if}
-
-        <div id="{$d.entity_table}_{$d.entity_id}" class="{$d.entity_table} Detail">
-            {if $d.entity_table ne "Page"}
-                <span class='handle'>ↈ</span>
-            {/if}
-            <input type="hidden" name="data[{$d.order}][order]" value="{$d.order}" class="order"/>
-            <input type="hidden" name="data[{$d.order}][entity_table]" value="{$d.entity_table}" />
-            {if $d.entity_table eq "jQuery"}
-                <label class="path">Elements:</label> <input class='jQuery-Selector' name="data[{$d.order}][entity_id]" value="{$d.entity_id}" />
-            {else}
-                <input type="hidden" name="data[{$d.order}][entity_id]" value="{$d.entity_id}" />
-                <div class='entity_name'>{$d.name}</div>
-            {/if}
-            <span class='DeleteStep'>delete</span>
-            <br />
-            <label class="leftmost">Breadcrumb:</label> <input name="data[{$d.order}][breadcrumb]" value="{$d.breadcrumb}" />
-            <label>Button Text:</label> <input name="data[{$d.order}][next]" value="{$d.next}" />
-            <label>Title:</label> <input name="data[{$d.order}][title]" value="{$d.title}" size='40' />
-        </div>
-
-    {/foreach}
-    </div>
+        <div id="SortableDetails"></div>
     </form>
 
     <div id="WorkflowStepTemplates">
