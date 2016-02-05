aabb6feea8cdff013226a0c3d7829e1af5f82358
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-02-05 02:42:06 -0800

    Update case code to properly handle new fields

--- js/workflow_steps_case.js
+++ js/workflow_steps_case.js
@@ -8,7 +8,9 @@ function SimpleWorkflowStepAddCase(template, index, data) {
     data.title = data.entity_name;
     data.next = "Next";
     data.options = {
-      "include_profile": CRM.$("#SWCaseIncludeProfile").is(":checked")
+      "include_profile": CRM.$("#SWCaseIncludeProfile").is(":checked"),
+      "core_fields": "",
+      mode: "create"
     };
   }
 
@@ -18,7 +20,31 @@ function SimpleWorkflowStepAddCase(template, index, data) {
 
   template.find(".entity_name").html(data.entity_name);
 
-  template.find(".option_include_profile").prop("checked", data.options.include_profile);
+
+  var mode = template.find(".case_option_mode");
+  mode.attr("name", mode.attr("name").replace("#ORDER#", index));
+  mode.val(data.options.mode);
+
+  var cFields = template.find(".case_option_core_fields");
+  cFields.attr("name", cFields.attr("name").replace("#ORDER#", index));
+  cFields.val(data.options.core_fields);
+
+  var fieldsData = [
+   {id: "client_id", text: ts("Client ID")},
+   {id: "medium_id", text: ts("Activity Medium")},
+   {id: "activity_details", text: ts("Details")},
+   {id: "activity_subject", text: ts("Subject")},
+   {id: "status_id", text: ts("Case Status")},
+   {id: "start_date", text: ts("Case Start Date")},
+   {id: "duration", text: ts("Activity Duration")},
+   {id: "attachments", text: ts("Attachments")},
+  ];
+
+
+  cFields.select2({data: fieldsData, multiple: true});
+  template.find("div.case_option_core_fields").css("width", "75%");
+
+  template.find(".case_option_include_profile").prop("checked", data.options.include_profile);
 
   template.find(".entity_table").val("Case");
   template.addClass("Case");
--- templates/CRM/Workflow/Page/Steps/StepTypes_case.tpl
+++ templates/CRM/Workflow/Page/Steps/StepTypes_case.tpl
@@ -2,7 +2,21 @@
     <input type ="hidden" name="data[#ORDER#][entity_id]" class="entity_id" />
 
     <div class="SWStepField">
-        <input type ="checkbox" name="data[#ORDER#][options][include_profile]" class="option_include_profile" />
+        <input type ="checkbox" name="data[#ORDER#][options][include_profile]" class="case_option_include_profile" />
         <label>{ts}Include Profile/Custom Data{/ts}</label>
     </div>
+
+    <div class="SWStepField">
+        <label>{ts}Mode{/ts}:</label>
+        <select name="data[#ORDER#][options][mode]" class="case_option_mode">
+            <option value="create">{ts}Create{/ts}</option>
+            <option value="edit">{ts}Edit{/ts}</option>
+        </select>
+    </div>
+
+    <div class="SWStepField">
+        <label>{ts}Fields to Include{/ts}:</label>
+        <input name="data[#ORDER#][options][core_fields]" class="case_option_core_fields" />
+    </div>
+
 </div>
