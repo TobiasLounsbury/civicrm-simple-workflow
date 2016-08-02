49d84c8a2cfd83b23ad3cf993182889a42dda6b3
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-08-02 15:17:43 -0700

    Added call to completeStep hook from within complete step api so that we can abstract some of the functionality we've already written

--- api/v3/Workflow/CompleteStep.php
+++ api/v3/Workflow/CompleteStep.php
@@ -9,17 +9,20 @@ function _civicrm_api3_workflow_complete_step_spec(&$spec) {
 
 function civicrm_api3_workflow_complete_step($params) {
 
+  $step = CRM_Workflow_BAO_WorkflowDetail::getDetail($params['workflow_id'], $params['step']);
 
   if(array_key_exists("contact", $params)) {
-    $key = "workflow_{$params['workflow_id']}_step_{$params['step']}_contact_id";
+    $key = "workflow_{$params['workflow_id']}_step_{$step['order']}_contact_id";
     CRM_Core_Session::singleton()->set($key, $params['contact'], "SimpleWorkflow");
   }
 
   if(array_key_exists("data", $params)) {
-    $key = "workflow_{$params['workflow_id']}_step_{$params['step']}_data";
+    $key = "workflow_{$params['workflow_id']}_step_{$step['order']}_data";
     CRM_Core_Session::singleton()->set($key, $params['data'], "SimpleWorkflow");
   }
 
+  $context = "api";
+  CRM_Workflow_hook::completeStep($params['workflow_id'], $params['step'], $context);
 
   return civicrm_api3_create_success("Success", $params, 'Workflow', 'completeStep');
 }
